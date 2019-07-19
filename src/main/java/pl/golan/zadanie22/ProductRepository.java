4c96b15eeec91f98bb541a7ad59336e05e1507ef
Grzegorz <grzegorzgolan18@gmail.com>
2019-07-19 12:22:57 +0200

    2

---
+++ src/main/java/pl/golan/zadanie22/ProductRepository.java
@@ -0,0 +1,47 @@
+package pl.golan.zadanie22;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Repository
+public class ProductRepository {
+    private Set<Product> products;
+    private double sum=0;
+
+    public ProductRepository() {
+        products = new HashSet<>();
+        products.add(new Product("Ziemniak", 13.42));
+        products.add(new Product("Pomidor", 23.32));
+      }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public Product findByName(String name) {
+        for (Product Product : products) {
+            if(Product.getName().equals(name)) {
+                return Product;
+            }
+        }
+
+        return null;
+    }
+
+    public Set<Product> findAll() {
+        return products;
+    }
+    public double sum(){
+        double suma = 0;
+        for (Product product : products) {
+            suma+=product.getPrice();
+        }
+        return suma;
+    }
+}
---
+++ src/main/java/pl/golan/zadanie22/ShopController.java
@@ -0,0 +1,57 @@
+package pl.golan.zadanie22;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
+
+@Controller
+public class ShopController {
+private ProductRepository productRepository;
+
+    public ShopController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/")
+    public String shop(Model model){
+        Set<Product> products = productRepository.findAll();
+        model.addAttribute("products",  products);
+        return "main";
+    }
+
+    @GetMapping("/lista")
+    public String details(Model model) {
+        Set<Product> products = productRepository.findAll();
+        double sum=0;
+        for (Product product : products) {
+            sum+=product.getPrice();
+        }
+
+        if(products != null) {
+            model.addAttribute("products", products);
+            return "lista"; // -> /resources/templates/lista.html
+        } else  {
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/tabela")
+    public String table(Model model) {
+        Set<Product> products = productRepository.findAll();
+        double sum=0;
+        for (Product product : products) {
+            sum+=product.getPrice();
+        }
+
+        if(products != null) {
+            model.addAttribute("products", products);
+            model.addAttribute("sum",sum);
+            return "tabela"; // -> /resources/templates/lista.html
+        } else  {
+            return "redirect:/";
+        }
+    }
+}
